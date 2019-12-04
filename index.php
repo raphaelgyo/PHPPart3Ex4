0415c80790fe8d537aedd6e47740fbee48118bb8
raphaelgyo <raphael.granizo@gmail.com>
2019-12-04 18:23:41 +0100

    1st attempt

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+  $departments = [02 => 'Aisne', 59 => 'Nord', 60 => 'Oise', 62 => 'Pas-de-calais', 80 => 'Somme'];
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Partie 3 Exercice 4 : Les tableaux</title>
+  </head>
+  <body>
+    
+  </body>
+</html>
