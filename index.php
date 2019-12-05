ddaf8ce10c07633771748e5c697eb464e447ed34
raphaelgyo <raphael.granizo@gmail.com>
2019-12-05 09:49:28 +0100

    correction

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
 <?php
-  $departments = [02 => 'Aisne', 59 => 'Nord', 60 => 'Oise', 62 => 'Pas-de-calais', 80 => 'Somme'];
+  $departments = ['02' => 'Aisne', '59' => 'Nord', '60' => 'Oise', '62' => 'Pas-de-calais', '80' => 'Somme'];
+?>
+<?php
+  $departments = array(
+    '02' => 'Aisne',
+    '59' => 'Nord',
+    '60' => 'Oise',
+    '62' => 'Pas-de-calais',
+    '80' => 'Somme');
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8" />
     <title>PHP Partie 3 Exercice 4 : Les tableaux</title>
   </head>
   <body>
-    
   </body>
 </html>
